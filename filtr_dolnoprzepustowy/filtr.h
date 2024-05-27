71edbdc5888166994cc258de6a21224cd55f854e
Joanna <162314255+Joanna610@users.noreply.github.com>
2024-05-27 13:33:22 +0200

    Filtr dolnoprzepustowy - pliki z kodem

---
+++ filtr_dolnoprzepustowy/filtr.h
@@ -0,0 +1,20 @@
+#ifndef __filtr_H
+#define __filtr_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "stdint.h"
+#include "filtr_struct.h"
+
+#define LOW_PASS_ALPHA	(0.1f)
+
+// lowpass_filter_t filter;
+
+void filter_lowpass_init(lowpass_filter_t *filter);
+int16_t filter_lowpass(int16_t new_data,  lowpass_filter_t *filter);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ tim_H */
---
+++ filtr_dolnoprzepustowy/filtr_struct.h
@@ -0,0 +1,17 @@
+#ifndef __filtr_struct_H
+#define __filtr_struct_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+ typedef struct
+ {
+ 	float out;
+ 	float alpha;
+ } lowpass_filter_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
